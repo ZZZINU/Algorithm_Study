06d1c37422120de79afbb531fe0412cb9625136e
이유진 <98758209+ZZZINU@users.noreply.github.com>
2024-11-17 11:32:17 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -6,12 +6,12 @@ def solution(n, computers):
         if visited[com] == False:
             DFS(n, computers, com, visited)
             answer += 1
-    
+            
     return answer
-
+            
 def DFS(n, computers, com, visited):
     visited[com] = True
     for connect in range(n):
-        if connect != com and computers[com][connect] == 1:
+        if connect != com and computers[com][connect] ==1:
             if visited[connect] == False:
                 DFS(n, computers, connect, visited)
