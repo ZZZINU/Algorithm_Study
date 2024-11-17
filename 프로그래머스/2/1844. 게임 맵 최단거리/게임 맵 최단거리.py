f7a9c423e44065f276bae518487d13dae147daea
이유진 <98758209+ZZZINU@users.noreply.github.com>
2024-11-17 11:27:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,27 @@
 from collections import deque
 def solution(maps):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dx = [-1,1,0, 0]
+    dy =[0, 0, -1, 1]
     n = len(maps)
     m = len(maps[0])
     
-    visited = [[False] * m for i in range(n)]
+    visited = [[False] * m for _ in range(n)]
     queue = deque()
-    queue.append((0, 0))
+    queue.append((0,0))
     
-    while queue:
+    while(queue):
         x, y = queue.popleft()
         
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-            if 0 <= nx < n and 0<= ny <m:
-                if visited[nx][ny] == False and maps[nx][ny] == 1:
+            if 0<=nx<n and 0<=ny<m:
+                if visited[nx][ny] == False and maps[nx][ny] ==1:
                     visited[nx][ny] = True
-                    maps[nx][ny] = maps[x][y]+ 1
+                    maps[nx][ny] = maps[x][y]+1
                     queue.append((nx, ny))
-        
     if maps[n-1][m-1] == 1:
         return -1
     else:
         return maps[n-1][m-1]
-    
-                
