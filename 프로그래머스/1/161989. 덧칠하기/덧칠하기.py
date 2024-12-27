111c3fbf77683c6d8e69997896e6eda8ea9fbaf8
이유진 <98758209+ZZZINU@users.noreply.github.com>
2024-12-28 00:11:47 +0900

    [level 1] Title: 덧칠하기, Time: 8.62 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,20 +1,11 @@
 def solution(n, m, section):
     answer = 0
-#     paint_list = [True] * (n+1)
     
-#     for item in section:
-#         paint_list[item] = False
-    
-#     print(paint_list)
     start = section[0]
     
     for i in range(1, len(section)):
         if section[i] - start >= m:
             start = section[i]
             answer += 1
-    
-            
-    
-        
     
     return answer + 1
