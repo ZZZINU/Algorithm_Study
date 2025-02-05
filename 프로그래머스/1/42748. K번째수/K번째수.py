ac320079df01b1f3cd1f4ed2807b298fdc68728c
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-02-05 23:46:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,11 @@
 def solution(array, commands):
     answer = []
+    
     for command in commands:
-        start, end, idx = command[0], command[1], command[2]
-        temp =  array[start-1: end]
+        start, end, idx = command
+        temp = array[start-1:end]
         temp.sort()
         answer.append(temp[idx-1])
-        
+
 
     return answer
