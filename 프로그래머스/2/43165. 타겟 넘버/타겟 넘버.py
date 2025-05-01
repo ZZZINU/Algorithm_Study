0171846b6b96055e42e8754a4c70277af0c8c1ec
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-05-01 21:59:56 +0900

    [level 2] Title: 타겟 넘버, Time: 453.60 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,17 @@
 answer = 0
 def DFS(numbers, target, step, value):
     global answer
-    if len(numbers) == step and target == value:
-        answer+=1
-        return
-    
     if len(numbers) == step:
-        return 
+        if value == target:
+            answer += 1
+            return 
+        else:
+            return
+        
         
     
-    DFS(numbers, target, step+1, value + numbers[step])
-    DFS(numbers, target, step+1, value - numbers[step])
+    DFS(numbers, target, step+1, value+numbers[step])
+    DFS(numbers, target, step+1, value-numbers[step])
 
 def solution(numbers, target):
     global answer
