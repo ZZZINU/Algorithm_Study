5eccbec72b38c51ee73ecd6f7e5e0c2e4b7ae23c
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-08-15 14:49:51 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.31 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,25 +1,26 @@
+from collections import deque
+    
 def solution(board, moves):
     answer = 0
-    basket = []
-    new_board = [[] for _ in range(len(board[0]))]
-
-    # board를 회전하여 new_board로 변환
-    for i in range(len(board) - 1, -1, -1):
-        for j in range(len(board[0])):
-            if board[i][j] != 0:
+    n = len(board)
+    new_board = [deque() for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]:
                 new_board[j].append(board[i][j])
-
-    # moves를 처리
+            
+    basket = []
+    count = 0
+    
     for move in moves:
-        # move는 1-based index이므로 0-based로 변환
-        column = move - 1
-        if new_board[column]:  # 해당 열에 인형이 남아 있는 경우
-            doll = new_board[column].pop()  # 인형을 뽑음
-            # basket의 마지막 인형과 비교
+        if new_board[move-1]:
+            doll = new_board[move-1].popleft()
             if basket and basket[-1] == doll:
-                basket.pop()  # 동일하면 제거
-                answer += 2  # 인형 2개 제거
+                basket.pop()
+                count += 1
             else:
-                basket.append(doll)  # 그렇지 않으면 추가
+                basket.append(doll)
+        
 
-    return answer
+
+    return count * 2
