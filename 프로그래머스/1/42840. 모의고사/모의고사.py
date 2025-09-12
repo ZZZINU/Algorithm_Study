861f77aac641a6ee4b1630dd91fd60da9f875cbf
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-09-12 19:22:28 +0900

    [level 1] Title: 모의고사, Time: 2.18 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,31 @@
 def solution(answers):
-    result= []
-    answer = [0, 0, 0]
-    person1 = [1, 2, 3, 4, 5]
-    person2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = []
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    first_score = 0
+    second_score = 0
+    third_score = 0
     
     for i in range(len(answers)):
-        if answers[i] == person1[i%5]:
-            answer[0] += 1
-        if answers[i] == person2[i%8]:
-            answer[1] += 1
-        if answers[i] == person3[i%10]:
-            answer[2] += 1
+        dap = answers[i]
+        if first[i%5] == dap:
+            first_score += 1
+        if second[i%8] == dap:
+            second_score += 1
+        if third[i%10] == dap:
+            third_score += 1
+        
+    max_score = max(first_score, second_score, third_score)
+    if max_score == first_score:
+        answer.append(1)
+    if max_score == second_score:
+        answer.append(2)
+    if max_score == third_score:
+        answer.append(3)
     
-    max_val = max(answer)
-    for idx, item in enumerate(answer):
-        if item == max_val:
-            result.append(idx+1)
+    
         
-        
-    return result
+    
+    return answer
