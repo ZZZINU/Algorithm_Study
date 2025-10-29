246b9ac2ca2a08cc02b02d639182e1d2363100ed
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-10-29 23:00:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.14 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,25 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    else:
-        return participant[-1]
+    answer = ''
+    hash_map = {}
+    
+    for person in completion:
+        if person not in hash_map:
+            hash_map[person] = 1
+        else:
+            hash_map[person] += 1
             
+    for person in participant:
+        if person not in hash_map:
+            return person
+        
+        if person in hash_map:
+            if hash_map[person] == 0:
+                return person
+            else:
+                hash_map[person] -= 1
+                
+            
+        
+        
+    
+    return answer
