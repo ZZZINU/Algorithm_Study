0da181f49a57075410ad126162ee57daa3e285a2
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-10-31 17:51:00 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,26 +1,13 @@
-def find_factor(num):
-    
-    nums = []
-    
-    for i in range(1, int(num**0.5)+1):
-        if num % i == 0:
-            nums.append(i)
-            nums.append(num // i)
-    return nums
-
 def solution(brown, yellow):
-    
     answer = []
-    count = brown + yellow
-    factors = find_factor(yellow)
+    x_y_sum = int((brown-4) / 2)
+    # print(x_y_sum)
+    x_y_multi = yellow
     
-    for i in range(0, len(factors), 2):
-        width = factors[i]
-        height = factors[i+1]
-        
-        if (width + height) * 2 + 4 == brown:
-            answer.append(height + 2)
-            answer.append(width + 2)
-            break
-    
-    return answer
+    for i in range(1, x_y_sum+1):
+        x = i
+        y = x_y_sum-x
+        if x * y == x_y_multi:
+            answer.append(max(x, y) + 2)
+            answer.append(min(x, y) + 2)
+            return answer
