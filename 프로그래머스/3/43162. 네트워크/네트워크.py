eb521b6c08290a90dd53984cdc3a82a08d2f9a0b
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-10-31 22:29:11 +0900

    [level 3] Title: 네트워크, Time: 1.69 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,37 +1,37 @@
 from collections import defaultdict
 def solution(n, computers):
     answer = 0
-    tree = defaultdict(list)
-    # 연결먼저
+    hash_map = defaultdict(list)
+    
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if i == j:
-                continue
-            
-            if computers[i-1][j-1] == 1:
-                tree[i].append(j)
+            if i != j and computers[i-1][j-1] == 1:
+                hash_map[i].append(j)
     
-    visited = [False] * n
+
+    visited = [False] * (n+1)
     
     for i in range(1, n+1):
-        
-        if visited[i-1]:
+        stack = []
+        if visited[i]:
             continue
-        
-        stack = [i]
-        visited[i-1] = True
+        answer += 1
+        stack.append(i)
+        visited[i] = True
         
         while stack:
-            node = stack.pop()
-            if node in tree:
-                for item in tree[node]:
-                    if not visited[item-1]:
-                        stack.append(item)
-                        visited[item-1] = True
+            now = stack.pop()
+
+            for item in hash_map[now]:
+                if not visited[item]:
+                    stack.append(item)
+                    visited[item] = True
+                
         
-        answer += 1
-        
+    
             
+            
+        
         
     
     return answer
