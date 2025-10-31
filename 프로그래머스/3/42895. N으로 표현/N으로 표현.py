fe3f6ffbc8945b40140a2fb46867d944abfc0638
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-10-31 21:55:20 +0900

    [level 3] Title: N으로 표현, Time: 17.33 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,22 @@
+def solution(N, number):
+
+    dp = [set() for _ in range(9)]
+    
+    for k in range(1, 9):
+        dp[k].add(int(str(N)*k))
+        
+        for i in range(1, k):
+            for a in dp[i]:
+                for b in dp[k-i]:
+                    dp[k].add(a+b)
+                    dp[k].add(a-b)
+                    dp[k].add(a*b)
+                    if b != 0:
+                        dp[k].add(a//b)
+        if number in dp[k]:
+            return k
+                    
+        
+    
+    
+    return -1
