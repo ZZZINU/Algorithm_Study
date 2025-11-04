a21c1163e8cd2f7903d8c23148c51b1dea58b40f
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-11-04 15:38:44 +0900

    [level 2] Title: 주식가격, Time: 23.85 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,19 @@
 from collections import deque
 def solution(prices):
-    # 이 부분을 채워주세요!
-    prices_queue = deque(prices)
-    result = []
-    while prices_queue:
-        price_not_fall_period = 0
-        current_price = prices_queue.popleft()
-        for next_price in prices_queue:
-            if current_price <= next_price:
-                price_not_fall_period += 1
-            else:
-                price_not_fall_period += 1
-                break
-        result.append(price_not_fall_period)
-
-    return result
+    n = len(prices)
+    answer = [0] * n
+    stack = []
+    
+    for i, price in enumerate(prices):
+        # 가격이 낮아진 경우
+        while stack and prices[stack[-1]] > price:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    
+    while stack:
+        j = stack.pop()
+        answer[j] = n - j - 1
+    
+    
+    return answer
