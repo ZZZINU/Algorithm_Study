85b84ab0e1d59b46adb2536ac6e4ecf9942e0fb1
이유진 <98758209+ZZZINU@users.noreply.github.com>
2025-11-04 17:10:47 +0900

    [level 3] Title: 정수 삼각형, Time: 33.30 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,21 +1,19 @@
 def solution(triangle):
-
+    answer = 0
     n = len(triangle)
-    dp = []
-    dp.append(triangle[0][0])
-
-
+    dp = triangle[0]
+    
     for i in range(1, n):
         dist = triangle[i]
-
-        dist[0] += dp[0] 
-        dist[-1] += dp[-1] 
         
-        for j in range(1, i):
+        dist[0] += dp[0]
+        dist[-1] += dp[-1]
+        
+        for j in range(1, len(dist)-1):
             dist[j] += max(dp[j-1], dp[j])
         
         dp = dist
-        
     
+    print(dp)
     
-    return max(dist)
+    return max(dp)
